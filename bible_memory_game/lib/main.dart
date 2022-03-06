e38703993daad8a0e5a4031cc50c20167c56ca22
JoeKhai <jmunian123@gmail.com>
2022-03-06 16:23:17 +0200

    init project + splash screen

---
+++ bible_memory_game/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bible_memory_game/screens/memory_game_screen.dart';
+import 'package:bible_memory_game/utils/game_utils.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MemoryGameScreen(),
+    );
+  }
+}
---
+++ bible_memory_game/lib/screens/memory_game_screen.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+import '../utils/game_utils.dart';
+
+class MemoryGameScreen extends StatefulWidget {
+  const MemoryGameScreen({Key? key}) : super(key: key);
+
+  @override
+  _MemoryGameScreenState createState() => _MemoryGameScreenState();
+}
+
+class _MemoryGameScreenState extends State<MemoryGameScreen> {
+  //setting text style
+  TextStyle whiteText = const TextStyle(color: Colors.white);
+  bool hideTest = false;
+  final Game _game = Game();
+
+  //game stats
+  int tries = 0;
+  int score = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _game.initGame();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 24.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              infoCard("Tries", "$tries"),
+              infoCard("Score", "$score"),
+            ],
+          ),
+          SizedBox(
+              height: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+              child: GridView.builder(
+                  itemCount: _game.gameImg!.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 16.0,
+                    mainAxisSpacing: 16.0,
+                  ),
+                  padding: const EdgeInsets.all(16.0),
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        print(_game.matchCheck);
+                        setState(() {
+                          //incrementing the clicks
+                          tries++;
+                          _game.gameImg![index] = _game.cards_list[index];
+                          _game.matchCheck
+                              .add({index: _game.cards_list[index]});
+                          print(_game.matchCheck.first);
+                        });
+                        if (_game.matchCheck.length == 2) {
+                          if (_game.matchCheck[0].values.first ==
+                              _game.matchCheck[1].values.first) {
+                            print("true");
+                            //incrementing the score
+                            score += 100;
+                            _game.matchCheck.clear();
+                          } else {
+                            print("false");
+
+                            Future.delayed(const Duration(milliseconds: 500),
+                                    () {
+                                  print(_game.gameColors);
+                                  setState(() {
+                                    _game.gameImg![_game.matchCheck[0].keys
+                                        .first] =
+                                        _game.hiddenCardpath;
+                                    _game.gameImg![_game.matchCheck[1].keys
+                                        .first] =
+                                        _game.hiddenCardpath;
+                                    _game.matchCheck.clear();
+                                  });
+                                });
+                          }
+                        }
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFFFB46A),
+                          borderRadius: BorderRadius.circular(8.0),
+                          image: DecorationImage(
+                            image: AssetImage(_game.gameImg![index]),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    );
+                  }))
+        ],
+      ),
+    );
+  }
+
+  Widget infoCard(String title, String info) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(26.0),
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 26.0),
+        decoration: const BoxDecoration(
+          color: Colors.green,
+        ),
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 6.0,
+            ),
+            Text(
+              info,
+              style:
+              const TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
